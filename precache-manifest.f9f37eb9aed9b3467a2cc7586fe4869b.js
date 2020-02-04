13bc015aef14e997a715d6c856c659a94c8d4d1f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-04 06:37:52 +0000

    Deploy Shohin217/Tariffs to github.com/Shohin217/Tariffs.git:gh-pages

---
+++ precache-manifest.f9f37eb9aed9b3467a2cc7586fe4869b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12a4f1368334ee344b7abe3ab17c486",
+    "url": "/Tariffs/index.html"
+  },
+  {
+    "revision": "c7da722a9e93015ba446",
+    "url": "/Tariffs/static/css/main.c59bb951.chunk.css"
+  },
+  {
+    "revision": "1f8db739107c662b3dbb",
+    "url": "/Tariffs/static/js/2.6d0e73af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tariffs/static/js/2.6d0e73af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7da722a9e93015ba446",
+    "url": "/Tariffs/static/js/main.6dfd97da.chunk.js"
+  },
+  {
+    "revision": "8d278c23f4aa8c39f5fe",
+    "url": "/Tariffs/static/js/runtime-main.dc7b9002.js"
+  }
+]);
